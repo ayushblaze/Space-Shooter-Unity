65f6f3be3adbeb99fa3c38f4f82c43149bc8beba
Ayush Singh <ayushmoca@gmail.com>
2023-09-23 15:50:40 +0530

    Refactoring

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,18 +7,21 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // take the current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
         transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
 
@@ -26,7 +29,7 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, 0, 0);
         } else if (transform.position.y <= -3.4f) {
             transform.position = new Vector3(transform.position.x, -3.4f, 0);
-        } 
+        }
 
         if (transform.position.x >= 11.3f) {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
