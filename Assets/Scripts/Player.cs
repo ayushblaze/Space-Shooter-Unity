d25ba9e7331a0049b24816fc3f483e7e9a7d467b
Ayush Singh <ayushmoca@gmail.com>
2023-09-25 19:45:20 +0530

    Cleaned up code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
     [SerializeField]
     public GameObject _laserPrefab;
     [SerializeField]
-    private float _fireRate = 0.5f;
+    private float _fireRate = 0.15f;
     private float _canFire = -1f;
 
     void Start()
@@ -20,10 +20,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
-        
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire) {
-            _canFire = Time.time + _fireRate;
-            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.85f, 0), Quaternion.identity);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
         }
     }
 
@@ -42,5 +42,11 @@ public class Player : MonoBehaviour
         } else if (transform.position.x < -11.3f) {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.85f, 0), Quaternion.identity);
     }
 }
