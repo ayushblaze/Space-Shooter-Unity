f639ec571e22beabea00c3980091a2a201b50727
Ayush Singh <ayushmoca@gmail.com>
2024-01-01 07:52:16 +0530

    implement powerups

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    public float _speed = 3.5f;
+    // IDs for Powerups
+    // 0 = Triple Shot
+    // 1 = Speed
+    // 2 = Shields
+    [SerializeField]
+    private int powerupID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -4.5f) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+            {
+                switch (powerupID)
+                {
+                    case 0:
+                        player.TripleShotActive();
+                        break;
+                    case 1:
+                        Debug.Log("Speed Powerup");
+                        break;
+                    case 2:
+                        Debug.Log("Shields");
+                        break;
+                    default:
+                        Debug.Log("Default Valur");
+                        break;
+                }
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
