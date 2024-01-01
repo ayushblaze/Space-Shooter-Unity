c319bcd16cb6a39923b632cbfe8bf0af037a960f
Ayush Singh <ayushmoca@gmail.com>
2024-01-01 07:11:58 +0530

    Added coroutines for spawing enemies and powerups

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,17 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    [SerializeField]
+    private GameObject _tripleShotPowerupPrefab;
+    private bool _stopSpawning = false;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
     }
 
     // Update is called once per frame
@@ -18,12 +25,28 @@ public class SpawnManager : MonoBehaviour
         
     }
 
-    //spawn game objects every 5 seconds
+    // Spawn game objects every 5 seconds
+    // Create a coroutine of type IEnumerator -- Yield Events
+    // While loop
+
     IEnumerator SpawnRoutine() {
-        while(true) {
+        while (_stopSpawning == false) { 
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
-            Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
         }
+    }
+
+    IEnumerator SpawnPowerupRoutine() {
+        while (_stopSpawning == false) {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newPowerup = Instantiate(_tripleShotPowerupPrefab, posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(3, 8));
+        }
+    }
+
+    public void OnPlayerDeath() {
+        _stopSpawning = true;
     }
 }
